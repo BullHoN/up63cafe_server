149b4e9312a2c67b1d1a5c60fbc62d3a2f99ace3
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2020-01-22 12:06:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname,'/landing_page/index.html'))
 })
 
-const PORT = 5000;
+const PORT = 8080;
 
 app.listen(PORT,()=>{
 	console.log("server is running at port 5000")
